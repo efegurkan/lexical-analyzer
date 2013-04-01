16f67e8df2889f5e0d9b5163aa9bd83b2725fd67
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2013-04-02 00:07:31 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/*Charachter types*/
+#define LETTER 0
+#define OPERATOR 1
+
+/*Token Codes*/
+#define IDENTIFIER 10
+#define ASSIGNMENT 11
+
+/*Globals*/
+int charType;
+char nextChar;
+
+int main()
+{
+    return 0;
+}
+
+void getCh()
+{
+    if((nextChar = getc(stw_fp)) != EOF)
+    {
+        if(isalpha(nextChar))
+            charType = LETTER;
+        else charType = OPERATOR;
+    }
+    else charType = EOF;
+
+}
+
+void cleanSpace()
+{
+    while (isspace(nextChar))
+        getCh();
+}
