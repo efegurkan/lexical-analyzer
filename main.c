37a263abc93b47619273caad6b89f90f3091140c
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2013-04-02 22:44:08 +0300

    small fixes and new function lex()
    New function lex. Parse for identifiers.

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 
 /*Charachter types*/
 #define LETTER 0
-#define OPERATOR 1
+#define DIGIT 1
+#define OTHER 2
 
 /*Token Codes*/
-#define IDENTIFIER 10
-#define ASSIGNMENT 11
+#define STR_LIT 10
+#define IDENTIFIER 11
+#define ASSIGNMENT 20
+
 
 /*Globals*/
 int charType;
 int nextToken;
+int lexLength;
 char nextChar;
 FILE *stw_fp, *fopen();
 
@@ -20,12 +24,13 @@ FILE *stw_fp, *fopen();
 void getCh();
 void cleanSpace();
 int opTokenizer(char ch);
+int lex();
 
 int main()
 {
-    if((stw_fp=fopen("test.stw","r")) == NULL)
+    if((stw_fp=fopen("test.stw","r")) == NULL)/*Try to open test.stw*/
         printf("E: Couldn't open test.stw\n");
-    else
+    else/*Success*/
     {
         getCh();
         do
@@ -36,18 +41,25 @@ int main()
     return 0;
 }
 
+/*Gets next char of input and determines its type*/
 void getCh()
 {
     if((nextChar = getc(stw_fp)) != EOF)
     {
         if(isalpha(nextChar))
             charType = LETTER;
-        else charType = OPERATOR;
+
+        else if (isdigit(nextChar))
+            charType = DIGIT;
+
+            else
+                charType = OTHER;
     }
     else charType = EOF;
 
 }
 
+/*Calls getCh until nextChar is not space*/
 void cleanSpace()
 {
     while (isspace(nextChar))
@@ -60,4 +72,28 @@ int opTokenizer(char ch)
 concat,trim,replace,eol
 assign */
     return 0;
+}
+
+int lex()
+{
+    lexLength = 0;
+    cleanSpace();
+
+    switch(charType)
+    {
+        /*Identifiers*/
+        case LETTER:
+            /* TODO (efegurkan#1#): add character */
+            getCh();
+            while (charType == LETTER || charType == DIGIT)
+            {
+                /* TODO (efegurkan#1#): add character */
+                getCh();
+            }
+            nextToken = IDENTIFIER;
+            break;
+
+    }/*End of switch*/
+
+    return nextToken;
 }
