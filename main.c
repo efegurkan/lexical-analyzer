c61bb862fbe8a68d56361438839df0f8c0b782fe
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2013-04-07 16:38:11 +0300

    exception handling and comment lines added

--- main.c
+++ main.c
@@ -20,14 +20,23 @@
 #define REPL_OP_COLON 27
 #define REPL_OP_LESS 28
 
-/*Error Codes*/
-#define LONG_ID 100
+/*Exception Codes*/
+#define EX_LONG_ID 100
+#define EX_STRING_UNCLOSED 101
+#define EX_COMMENT_UNCLOSED 102
+#define EX_ID_DIGIT 103
+#define EX_UNKNOWN 104
+
+/*Error codes*/
+#define ERROR_ID 105
+#define ERROR_UNCL 106
 
 /*Globals*/
 int charType;
 int nextToken;
 int lexLength;
 int lineNo = 0;
+int errorFlag = 0;
 char nextChar;
 char lexeme[20];
 long int position,position2;
@@ -39,11 +48,12 @@ void cleanSpace();
 int opTokenizer(char ch);
 int lex();
 void addCh();
-void goStablePosition();
+void goStablePosition(int errorCode);
+void exceptionHandler(int exception);
 
 int main()
 {
-    if((stw_fp=fopen("comment.stw","r")) == NULL)/*Try to open test.stw*/
+    if((stw_fp=fopen("error.stw","r")) == NULL)/*Try to open test.stw*/
         printf("E: Couldn't open test.stw\n");
     else/*Success*/
     {
@@ -52,7 +62,16 @@ int main()
         do
         {
             lex();
-        }while(nextToken != EOF);
+        }
+        while(nextToken != EOF);
+
+        fclose(stw_fp);
+        fclose(tkn_fp);
+
+        if (errorFlag != 0)
+        {
+            remove("tokens.tkn");
+        }
     }
     return 0;
 }
@@ -60,30 +79,48 @@ int main()
 /*Adds identifiers name to lexeme.*/
 void addCh()
 {
-    if ( lexLength <=19)
+    if (lexLength <=19)
     {
         lexeme[lexLength++] = nextChar;
         lexeme[lexLength] = 0;
     }
     else
     {
-        printf("E:Line %d: Identifier is too long!",lineNo);
-        goStablePosition(LONG_ID);
+        exceptionHandler(EX_LONG_ID);
     }
 }
 
+/*Tries to go to the next stable position according to error code*/
 void goStablePosition(int errorCode)
 {
-    /* TODO (efegurkan#1#): go to next stable position via proper Error Code */
+    getCh();
+    switch(errorCode)
+    {
+    case ERROR_ID:/*Identifier related errors*/
+        while (charType == LETTER || charType == DIGIT)
+        {
+            getCh();
+        }
+        break;
+
+    case ERROR_UNCL:/*Unclosed comment or string literal*/
+        while (nextChar== ';' || nextChar == EOF)
+        {
+            getCh();
+        }
+        break;
+    }
 }
 
+/*Writes string literals to the token file*/
 void stringLit()
 {
     do
     {
         putc(nextChar,tkn_fp);
         getCh();
-    }while(nextChar != '"');
+    }
+    while(nextChar != '"');
     putc(nextChar,tkn_fp);
     getCh();
 }
@@ -95,12 +132,10 @@ void getCh()
     {
         if(isalpha(nextChar))
             charType = LETTER;
-
         else if (isdigit(nextChar))
             charType = DIGIT;
-
-            else
-                charType = OTHER;
+        else
+            charType = OTHER;
     }
     else charType = EOF;
 
@@ -115,101 +150,153 @@ void cleanSpace()
         getCh();
 }
 
+/*Tries to find what is the next token.*/
 int opTokenizer(char ch)
 {
     switch(ch)
     {
-        case '(':
-           nextToken = LEFT_PAR;
-           break;
+    case '(':
+        nextToken = LEFT_PAR;
+        break;
 
-        case ')':
-            nextToken = RIGHT_PAR;
-            break;
+    case ')':
+        nextToken = RIGHT_PAR;
+        break;
 
-        case ';':
-            nextToken = EOL;
-            break;
+    case ';':
+        nextToken = EOL;
+        break;
 
-        case '+':
-            nextToken = CONCAT_OP;
-            break;
+    case '+':
+        nextToken = CONCAT_OP;
+        break;
 
-        case '/':
-            position = ftell(stw_fp);
-            getCh();
-            if(nextChar == '*')
-            {
-                do
-                {
-                    getCh();
-                    if(nextChar=='*')
-                    {
-                        position2 = ftell(stw_fp);
-                        getCh();
-                        if(nextChar == '/')
-                        {
-                            nextToken = 0;
-                            break;
-                        }
-                        else
-                            fseek(stw_fp,position2,SEEK_SET);
-                    }
-                }while(nextChar != EOF);
-//                if (nextChar == EOF )
-                   /* TODO (efegurkan#1#): error handling if nextChar == EOF */
-            }
-            else
-            {
-                nextToken = TRIM_OP;
-                fseek(stw_fp,position,SEEK_SET);
-            }
-            break;
-
-        case '<':
-            nextToken = REPL_OP_LESS;
-            break;
-
-        case ':':
-            position = ftell(stw_fp);/*Take current position for possible errors*/
-            cleanSpace();
-            getCh();
-
-            if (nextChar == '=')/*Assignment*/
-                nextToken = ASSIGNMENT_OP;
-            else if(nextChar == '/')/*Truncation*/
-                nextToken = TRUNC_OP;
-            else if (nextToken == '"')/*Replacement.In case of encountering with a string literal*/
-                {
-                    nextToken = REPL_OP_COLON;
-                    fseek(stw_fp,position,SEEK_SET);
-                }
-            /* TODO (efegurkan#1#): else error message */
-            break;
-
-        case '"':
-            position = ftell(stw_fp);
-
-            do
+    case '/':
+        position = ftell(stw_fp);
+        getCh();
+        if(nextChar == '*')/*Is it comment or Trimming OP*/
+        {
+            do/*Go through EOF*/
             {
                 getCh();
-            }while(nextChar!= EOF && nextChar != '"');
-
-            if (nextChar == '"')
-            {
-                fseek(stw_fp,position-1,SEEK_SET);
-//                stw_fp--;
-                nextToken = STR_LIT;
+                if(nextChar=='*')
+                {
+                    position2 = ftell(stw_fp);/*Take * character's position*/
+                    getCh();
+                    if(nextChar == '/')/*If there is / after * this is comment*/
+                    {
+                        nextToken = 0;/*Comments not included tokens*/
+                        break;
+                    }
+                    else
+                        fseek(stw_fp,position2,SEEK_SET);/*there is no / after * character*/
+                }
             }
-//            else if(nextChar == EOF)
-                /*Error message*/
-            break;
-        /* TODO (efegurkan#1#): Unknown charachter error handling. */
+            while(nextChar != EOF);
 
+            if (nextChar == EOF )/*Unclosed comment exception*/
+            {
+                exceptionHandler(EX_COMMENT_UNCLOSED);
+            }
+        }
+        else/*Trimming Operator*/
+        {
+            nextToken = TRIM_OP;
+            fseek(stw_fp,position,SEEK_SET);
+        }
+        break;
+
+    case '<':
+        nextToken = REPL_OP_LESS;
+        break;
+
+    case ':':
+        position = ftell(stw_fp);/*Take current position for possible errors*/
+        cleanSpace();
+        getCh();
+
+        if (nextChar == '=')/*Assignment*/
+            nextToken = ASSIGNMENT_OP;
+        else if(nextChar == '/')/*Truncation*/
+            nextToken = TRUNC_OP;
+        else if (nextToken == '"')/*Replacement.In case of encountering with a string literal*/
+        {
+            nextToken = REPL_OP_COLON;
+            fseek(stw_fp,position,SEEK_SET);
+        }
+        else
+        {
+            exceptionHandler(EX_UNKNOWN);/*If it doesn't match any*/
+        }
+        break;
+
+    case '"':/*String literal*/
+        position = ftell(stw_fp);/*For backtracing*/
+
+        /*Read through EOF or " */
+        do
+        {
+            getCh();
+        }
+        while(nextChar!= EOF && nextChar != '"');
+
+        if (nextChar == '"')/*No error Return to the startting point*/
+        {
+            fseek(stw_fp,position-1,SEEK_SET);
+            nextToken = STR_LIT;
+        }
+        else if(nextChar == EOF)/*Unclosed comment exception*/
+        {
+            exceptionHandler(EX_STRING_UNCLOSED);
+        }
+        break;
+
+    default:
+        exceptionHandler(EX_UNKNOWN);/*Unknown charachter*/
+        break;
     }
     return nextToken;
 }
 
+void exceptionHandler(int exception)
+{
+    switch(exception)
+    {
+    case EX_LONG_ID:
+        printf("E:Line %d: Identifier is too long!\n",lineNo);
+        goStablePosition(ERROR_ID);
+        break;
+
+    case EX_ID_DIGIT:
+        printf("E:Line %d: Identifiers MUST start with a letter!\n",lineNo);
+        goStablePosition(ERROR_ID);
+        break;
+
+    case EX_COMMENT_UNCLOSED:
+        printf("E:Line %d: Unclosed comment!\n",lineNo);
+        fseek(stw_fp,position,SEEK_SET);
+        goStablePosition(ERROR_UNCL);
+        nextToken = 0;
+        break;
+
+    case EX_STRING_UNCLOSED:
+        printf("E:Line %d: Unclosed String Literal!\n",lineNo);
+        fseek(stw_fp,position,SEEK_SET);
+        goStablePosition(ERROR_UNCL);
+        nextToken = 0;
+        break;
+
+    case EX_UNKNOWN:
+        printf("E:Line %d: Unknown charachter!\n",lineNo);
+        nextToken = 0;
+        break;
+
+    }
+
+    errorFlag = 1;
+}
+
+/*Looks lexemes and decides the token charachters.*/
 int lex()
 {
     lexLength = 0;
@@ -217,30 +304,30 @@ int lex()
 
     switch(charType)
     {
-        /*Identifiers*/
-        case LETTER:
+    /*Identifiers*/
+    case LETTER:
+        addCh();
+        getCh();
+        while (charType == LETTER || charType == DIGIT)
+        {
             addCh();
             getCh();
-            while (charType == LETTER || charType == DIGIT)
-            {
-                addCh();
-                getCh();
-            }
-            nextToken = IDENTIFIER;
-            break;
+        }
+        nextToken = IDENTIFIER;
+        break;
 
-        case DIGIT:
-            /* TODO (efegurkan#1#): Error handling */
-            break;
+    case DIGIT:
+        exceptionHandler(EX_ID_DIGIT);
+        break;
 
-        case OTHER:
-            opTokenizer(nextChar);
-            getCh();
-            break;
+    case OTHER:
+        opTokenizer(nextChar);
+        getCh();
+        break;
 
-        case EOF:
-            nextToken = EOF;
-            break;
+    case EOF:
+        nextToken = EOF;
+        break;
 
     }/*End of switch*/
     if (nextToken != 0)
@@ -249,12 +336,13 @@ int lex()
         if (nextToken == STR_LIT)
         {
             stringLit();
-        }else if (nextToken == IDENTIFIER)
-            {
-                putc('"',tkn_fp);
-                fputs(lexeme, tkn_fp);
-                putc('"',tkn_fp);
-            }
+        }
+        else if (nextToken == IDENTIFIER)
+        {
+            putc('"',tkn_fp);
+            fputs(lexeme, tkn_fp);
+            putc('"',tkn_fp);
+        }
         putc(',',tkn_fp);
     }
 
