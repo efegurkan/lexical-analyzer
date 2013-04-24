f0b54893f00e4607f9d543fd9696e190e8d39f6f
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2013-04-24 20:36:47 +0300

    Update main.c
    Added license information

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
+/*
+* Strawman Lexical Analyzer - A Homework About Programming Languages
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright © 2013 Efe Gürkan YALAMAN, Doğukan Sever, Burak Emre Dost
+*
+* Authors: Efe Gürkan Yalaman - efeyalaman [at] gmail [dot] com
+* Doğukan Sever - dogukansever [at] hotmail [dot] com
+* Burak Emre Dost - burakemredost [at] gmail [dot] com
+*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
